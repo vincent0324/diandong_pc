544b1f2794d97358fbf47439de44f0bffa6762dc
张帆 <fireworks_zf@163.com>
2016-11-30 18:00:56 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+    entry: {},
+
+    output: {},
+
+    module: {},
+
+    plugins: []
+};
+
+module.exports = config;
